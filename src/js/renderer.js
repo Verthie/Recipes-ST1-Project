ee5b791282cfcdf62ccb95652d432384b4ca9121
Verth <vertisixs@gmail.com>
2023-06-30 23:49:38 +0200

    final commit

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,22 +1,11 @@
 const { ipcRenderer } = require("electron");
 
-var knex = require("knex")({
-  client: "sqlite3",
-  connection: {
-    filename: "../../database.sqlite",
-  },
-});
-
 const iconsPath = require("path").join(__dirname, "../img/icons.svg");
 const icons = `file://${iconsPath}`;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const recipeContainer = document.querySelector(".recipe");
-  const previewContainer = document.querySelector(".results");
-  const searchInput = document.querySelector(".search__field");
-  let recipeLoadId = 1;
-  let recipesLoadTitle = "Kurczak Curry";
   function getRecipe(recipe_details) {
+    const recipeContainer = document.querySelector(".recipe");
     console.log("recipe_details:", recipe_details);
     const filteredIngredients = recipe_details.filter(
       (ingredient) => recipe_details.recipesId === recipe_details.id
@@ -50,27 +39,9 @@ document.addEventListener("DOMContentLoaded", () => {
               recipe_details[id].serving_size
             }</span>
             <span class="recipe__info-text">ilość porcji</span>
-
-            <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use xlink:href="${icons}#icon-minus-circle"></use>
-                </svg>
-              </button>
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use xlink:href="${icons}#icon-plus-circle"></use>
-                </svg>
-              </button>
-            </div>
           </div>
 
-          <div class="recipe__user-generated">
-            <svg>
-              <use xlink:href="${icons}#icon-user"></use>
-            </svg>
-          </div>
-          <button class="btn--round">
+          <button class="btn--round" style="margin-left: 250px;">
             <svg class="">
               <use xlink:href="${icons}#icon-bookmark-fill"></use>
             </svg>
@@ -102,130 +73,109 @@ document.addEventListener("DOMContentLoaded", () => {
         </div>`;
     recipeContainer.innerHTML = "";
     recipeContainer.insertAdjacentHTML("afterbegin", markup);
+
+    const bookmarkButton = recipeContainer.querySelector(".btn--round");
+    bookmarkButton.addEventListener("click", function () {
+      const recipeLoadId = recipe_details[id].recipesId;
+      console.log("recipeLoadId:", recipeLoadId);
+      ipcRenderer.send("bookmarkRecipe", recipeLoadId);
+    });
   }
 
-  ipcRenderer.send("mainWindowLoaded");
-  ipcRenderer.send("detailsRecipe", recipeLoadId);
-  ipcRenderer.send("leftBarRecipes", recipesLoadTitle);
+  function getResults(results) {
+    const previewContainer = document.querySelector(".results");
+    console.log("results:", results);
+    const filteredResults = results.filter(
+      (result) => results.id === results.id
+    );
+    const markup = `${filteredResults
+      .map(
+        (result) => `
+            <li class="preview">
+                <a class="preview__link preview__link--active" data-result-id="${result.id}" id="myButton" href="#">
+                  <figure class="preview__fig">
+                    <img src="${result.image_url}" alt="Test" />
+                  </figure>
+                  <div class="preview__data">
+                    <h4 class="preview__title">${result.title}</h4>
+                  </div>
+                </a>
+              </li>
+                        `
+      )
+      .join("")};`;
 
-  // ipcRenderer.on("allRecipesSent", function (evt, allrecipes) {
-  //   console.log(allrecipes);
-  // });
+    previewContainer.innerHTML = "";
+    previewContainer.insertAdjacentHTML("afterbegin", markup);
+
+    const buttons = previewContainer.querySelectorAll(".preview__link");
+    buttons.forEach((button) => {
+      button.addEventListener("click", function () {
+        const recipeLoadId = button.getAttribute("data-result-id");
+        ipcRenderer.send("detailsRecipe", recipeLoadId);
+      });
+    });
+  }
+
+  function getBookmarks(bookmarks) {
+    const bookmarkContainer = document.querySelector(".bookmarks__list");
+    console.log("bookmarks:", bookmarks);
+    const filteredBookmarks = bookmarks.filter(
+      (bookmark) => bookmarks.id === bookmarks.id
+    );
+    const markup = `${filteredBookmarks
+      .map(
+        (bookmark) => `
+            <li class="preview">
+                <a class="preview__link preview__link--active" data-result-id="${bookmark.id}" id="myButton" href="#">
+                  <figure class="preview__fig">
+                    <img src="${bookmark.image_url}" alt="Test" />
+                  </figure>
+                  <div class="preview__data">
+                    <h4 class="preview__title">${bookmark.title}</h4>
+                  </div>
+                </a>
+              </li>
+                        `
+      )
+      .join("")};`;
+
+    bookmarkContainer.innerHTML = "";
+    bookmarkContainer.insertAdjacentHTML("afterbegin", markup);
+
+    const buttons = bookmarkContainer.querySelectorAll(".preview__link");
+    buttons.forEach((button) => {
+      button.addEventListener("click", function () {
+        const recipeLoadId = button.getAttribute("data-result-id");
+        ipcRenderer.send("detailsRecipe", recipeLoadId);
+      });
+    });
+  }
+
+  document
+    .getElementById("searchInput")
+    .addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        event.preventDefault(); // Prevent the form from submitting
+        var recipesLoadTitle = document.getElementById("searchInput").value;
+        // var recipesLoadTitle = document.querySelector(".search__field").value;
+        ipcRenderer.send("leftBarRecipes", recipesLoadTitle);
+      }
+    });
+
+  ipcRenderer.send("mainWindowLoaded");
+  ipcRenderer.on("allBookmarkRecipesSent", function (evt, allrecipes) {
+    // console.log(allrecipes);
+    getBookmarks(allrecipes);
+  });
 
   ipcRenderer.on("recipeWithIgSent", function (evt, recipeWithIg) {
-    console.log("recipeWithIg:", recipeWithIg);
-    getRecipe(recipeWithIg);
+    // console.log("recipeWithIg:", recipeWithIg);
+    getRecipe(([results] = recipeWithIg));
   });
 
   ipcRenderer.on("recipesNoIgSent", function (evt, recipesNoIg) {
-    console.log("recipesNoIg:", recipesNoIg);
+    // console.log("recipesNoIg:", recipesNoIg);
+    getResults(recipesNoIg);
   });
-  /*
-
-  ipcRenderer.on("ingredientsSent", function (evt, ingredients) {
-    let all_ingredients = ingredients;
-    const recipeContainer = document.querySelector(".recipeIngredients");
-    function getIngredient(id) {
-      const markup = `
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Składniki</h2>
-          <ul class="recipe__ingredient-list">
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${all_ingredients[id].quantity}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${all_ingredients[id].unit}</span>
-                ${all_ingredients[id].name}
-              </div>
-            </li>
-          </div>
-            `;
-      recipeContainer.innerHTML = "";
-      recipeContainer.insertAdjacentHTML("afterbegin", markup);
-    }
-    getIngredient(0);
-  });
-  ipcRenderer.on("ingredientsSent", function (evt, ingredients) {
-    let all_ingredients = ingredients;
-    const recipeContainer = document.querySelector(".recipeIngredients");
-    function getIngredient(id) {
-      const markup = `
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Składniki</h2>
-          <ul class="recipe__ingredient-list">
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${all_ingredients[id].quantity}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${all_ingredients[id].unit}</span>
-                ${all_ingredients[id].name}
-              </div>
-            </li>
-          </div>
-            `;
-      recipeContainer.innerHTML = "";
-      recipeContainer.insertAdjacentHTML("afterbegin", markup);
-    }
-    for (let i = 1; i < 6; i++) {
-      getIngredient(i);
-    }
-  });
-  */
-
-  // ipcRenderer.on("ingredientsSent", function (evt, ingredients) {
-  //   let all_ingredients = ingredients;
-  //   const recipeContainer = document.querySelector(".recipeIngredients");
-
-  //   function getIngredients(recipeId) {
-  //     const filteredIngredients = all_ingredients.filter(
-  //       (ingredient) => ingredient.recipesId === recipeId
-  //     );
-
-  //     const markup = `
-  //     <div class="recipe__ingredients">
-  //       <h2 class="heading--2">Składniki</h2>
-  //       <ul class="recipe__ingredient-list">
-  //         ${filteredIngredients
-  //           .map(
-  //             (ingredient) => `
-  //             <li class="recipe__ingredient">
-  //               <svg class="recipe__icon">
-  //                 <use href="${icons}#icon-check"></use>
-  //               </svg>
-  //               <div class="recipe__quantity">${ingredient.quantity}</div>
-  //               <div class="recipe__description">
-  //                 <span class="recipe__unit">${ingredient.unit}</span>
-  //                 ${ingredient.name}
-  //               </div>
-  //             </li>
-  //           `
-  //           )
-  //           .join("")}
-  //       </ul>
-  //     </div>`;
-
-  //     recipeContainer.innerHTML = "";
-  //     recipeContainer.insertAdjacentHTML("afterbegin", markup);
-  //   }
-
-  //   // Call the function with the desired recipeId
-  //   getIngredients(1);
-
-  //   function createButtons() {
-  //     recipes.forEach((recipe) => {
-  //       const button = document.createElement("button");
-
-  //       button.textContent = recipe.name;
-  //       previewContainer.appendChild(button);
-
-  //       button.addEventListener("click", () => {
-  //         getRecipe(`${recipe.id}`);
-  //       });
-  //     });
-  //   }
-  // });
 });
